6ee016bd9f98c75601a0ba24d3e49109b0216582
alisker <balisker@mail.ru>
2025-01-15 23:30:54 +0300

    expo router

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+const Page = () => {
+	return (
+		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+			<Text>App</Text>
+		</View>
+	);
+};
+
+export default Page;
