1729c2b1f47a746150442366bc6cfab96621108f
alisker <balisker@mail.ru>
2025-02-01 12:27:27 +0300

    unistyles provider

--- src/app/root-layout/providers/unistyles/UnistylesProvider.tsx
+++ src/app/root-layout/providers/unistyles/UnistylesProvider.tsx
@@ -1,18 +1,6 @@
-import { BREAKPOINTS, darkTheme, lightTheme } from '@/shared/lib/unistyles';
 import React, { ReactNode } from 'react';
-import {
-	UnistylesProvider as RNUnistylesProvider,
-	UnistylesRegistry
-} from 'react-native-unistyles';
-
-UnistylesRegistry.addBreakpoints(BREAKPOINTS)
-	.addThemes({
-		light: lightTheme,
-		dark: darkTheme
-	})
-	.addConfig({
-		adaptiveThemes: true
-	});
+import { UnistylesProvider as RNUnistylesProvider } from 'react-native-unistyles';
+import './unistyles';
 
 export const UnistylesProvider = ({ children }: { children: ReactNode }) => (
 	<RNUnistylesProvider>{children}</RNUnistylesProvider>
