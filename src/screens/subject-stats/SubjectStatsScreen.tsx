286f26883f8875070763cf5736f8a0f0e4e56557
alisker <balisker@mail.ru>
2025-08-19 01:46:12 +0300

    replace cup icon

--- src/screens/subject-stats/SubjectStatsScreen.tsx
+++ src/screens/subject-stats/SubjectStatsScreen.tsx
@@ -1,13 +1,13 @@
 import { Text, View } from 'react-native';
 import { createStyleSheet, useStyles } from 'react-native-unistyles';
-import { CupFirstIcon } from '@/shared/assets/icons';
+import { TeaCupIcon } from '@/shared/assets/icons';
 
 export const SubjectStatsScreen = () => {
 	const { styles, theme } = useStyles(stylesheet);
 
 	return (
 		<View style={styles.container}>
-			<CupFirstIcon style={styles.icon} color={theme.colors.primary} />
+			<TeaCupIcon style={styles.icon} color={theme.colors.primary} />
 
 			<Text style={styles.text}>Совсем скоро...</Text>
 		</View>
@@ -20,16 +20,16 @@ const stylesheet = createStyleSheet(theme => ({
 		flex: 1,
 		justifyContent: 'center',
 		alignItems: 'center',
-		rowGap: 6
+		rowGap: 2
 	},
 	icon: {
 		width: 60,
 		height: 60
 	},
 	text: {
-		fontFamily: theme.typography.fontFamily.GolosTextMedium,
-		fontSize: 15,
+		textAlign: 'center',
 		color: theme.colors.primary,
-		textAlign: 'center'
+		fontSize: 15,
+		fontFamily: theme.typography.fontFamily.GolosTextMedium
 	}
 }));
