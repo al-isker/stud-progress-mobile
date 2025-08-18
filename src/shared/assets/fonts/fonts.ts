405416e1c5cbba1905683d183995ec8812e46d82
Alisker <alisker.babaev@pay-go.ru>
2025-08-18 17:34:08 +0300

    remove unused fonts

--- src/shared/assets/fonts/fonts.ts
+++ src/shared/assets/fonts/fonts.ts
@@ -2,5 +2,3 @@ export const GolosTextRegular = require('../../../../assets/fonts/GolosText-Regu
 export const GolosTextMedium = require('../../../../assets/fonts/GolosText-Medium.ttf');
 export const GolosTextSemiBold = require('../../../../assets/fonts/GolosText-SemiBold.ttf');
 export const GolosTextBold = require('../../../../assets/fonts/GolosText-Bold.ttf');
-export const GolosTextExtraBold = require('../../../../assets/fonts/GolosText-ExtraBold.ttf');
-export const GolosTextBlack = require('../../../../assets/fonts/GolosText-Black.ttf');
--- src/shared/lib/theme/tokens/font-family.ts
+++ src/shared/lib/theme/tokens/font-family.ts
@@ -1,12 +1,10 @@
 import * as Fonts from '@/shared/assets/fonts';
 
-type fontFamily = Record<keyof typeof Fonts, keyof typeof Fonts>;
+type FontFamilyType = Record<keyof typeof Fonts, keyof typeof Fonts>;
 
 export const FONT_FAMILY = {
 	GolosTextRegular: 'GolosTextRegular',
 	GolosTextMedium: 'GolosTextMedium',
 	GolosTextSemiBold: 'GolosTextSemiBold',
-	GolosTextBold: 'GolosTextBold',
-	GolosTextExtraBold: 'GolosTextExtraBold',
-	GolosTextBlack: 'GolosTextBlack'
-} satisfies fontFamily;
+	GolosTextBold: 'GolosTextBold'
+} satisfies FontFamilyType;
