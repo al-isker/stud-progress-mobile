726e5f82629a87f48551e6f6d92d271f22ae2db1
alisker <balisker@mail.ru>
2026-04-26 16:04:58 +0300

    use-logout-mutation hook import from public api

--- src/entities/auth/index.ts
+++ src/entities/auth/index.ts
@@ -1,4 +1,5 @@
 export { useLoginMutation } from './api/use-login-mutation';
+export { useLogoutMutation } from './api/use-logout-mutation';
 export { authInterceptors } from './config/auth-interceptors';
 export { AuthType } from './model/auth/auth-type';
 export { LoginBodyType } from './model/login/login-body-type';
--- src/features/logout/model/use-logout.ts
+++ src/features/logout/model/use-logout.ts
@@ -1,6 +1,6 @@
 import { router } from 'expo-router';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useLogoutMutation } from '@/entities/auth/api/use-logout-mutation';
+import { useLogoutMutation } from '@/entities/auth';
 import { routes } from '@/shared/config/navigation';
 import {
 	ACCESS_TOKEN_STORAGE_KEY,
